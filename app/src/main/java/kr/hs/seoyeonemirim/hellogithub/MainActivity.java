592a42d45c907309fdb778496ba3cbe7ab58719b
Oh Seoyeon <ohsy1018@e-mirim.hs.kr>
2017-03-30 20:21:26 +0900

    hellogithub

---
+++ app/src/main/java/kr/hs/seoyeonemirim/hellogithub/MainActivity.java
@@ -0,0 +1,10 @@
+package kr.hs.seoyeonemirim.hellogithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+}
